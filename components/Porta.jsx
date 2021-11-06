e82a4d7ef85c65169cede27927a3f4a035a55417
bruno3du <bruno.3duardo@hotmail.com>
2021-11-06 14:41:12 -0300

    Finalizado criacao de porta e presente

---
+++ components/Porta.jsx
@@ -0,0 +1,20 @@
+/** @format */
+
+import styles from '../styles/Porta.module.css';
+
+const Porta = (props) => {
+	const selecionada = props.selecionada ? styles.selecionada : '';
+	return (
+		<div className={styles.area}>
+			<div className={`${styles.estrutura} ${selecionada}`}>
+				<div className={styles.porta}>
+					<div className={styles.numero}>3</div>
+					<div className={styles.macaneta}></div>
+				</div>
+			</div>
+			<div className={styles.chao}></div>
+		</div>
+	);
+};
+
+export default Porta;
---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Porta from "../components/Porta";
+import Presente from "../components/Presente";
+
+export default function Home() {
+  return (
+    <div >
+      <Porta selecionada/>
+      <Porta />
+    </div>
+  )
+}
